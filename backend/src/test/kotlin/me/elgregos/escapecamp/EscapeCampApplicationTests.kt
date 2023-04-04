9531178adf1442c848b7c32beec80a49af7af2b4
Grégory BÉVAN <gregory.bevan@zenika.com>
2023-04-05 00:17:41 +0200

    💚 Enable as simple junit test

--- backend/src/test/kotlin/me/elgregos/escapecamp/EscapeCampApplicationTests.kt
+++ backend/src/test/kotlin/me/elgregos/escapecamp/EscapeCampApplicationTests.kt
@@ -1,11 +1,8 @@
 package me.elgregos.escapecamp
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-@Disabled
+//@SpringBootTest
 class EscapeCampApplicationTests {
 
 	@Test
