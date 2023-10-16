f59a14119b481cfe9d0f150d4a91a7005aebe0ca
Grégory BÉVAN <gregory.bevan@zenika.com>
2023-10-16 17:58:46 +0200

    ✨ Add service for Game Leaderboard

--- backend/src/test/kotlin/me/elgregos/escapecamp/game/application/GameServiceTest.kt
+++ backend/src/test/kotlin/me/elgregos/escapecamp/game/application/GameServiceTest.kt
@@ -2,10 +2,14 @@ package me.elgregos.escapecamp.game.application
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import io.mockk.every
 import io.mockk.mockk
-import me.elgregos.escapecamp.game.domain.entity.riddles
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import me.elgregos.escapecamp.game.domain.entity.*
+import me.elgregos.reakteves.domain.projection.ProjectionStore
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.time.Duration
+import java.util.*
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
@@ -13,31 +17,31 @@ class GameServiceTest {
 
     private lateinit var gameService: GameService
 
+    private lateinit var projectionStore: ProjectionStore<Game, UUID, UUID>
+
     @BeforeTest
     fun setUp() {
-        gameService = GameService(mockk(), riddles)
+        projectionStore = mockk()
+        gameService = GameService(projectionStore, riddles)
     }
 
     @Test
-    fun `should retrieve riddle content`() {
-        assertThat(gameService.retrieveRiddleContent("riddle-1"))
-            .isEqualTo("""
-            # A la piscine
+    fun `should get game leaderboard`() {
+        every { projectionStore.find(escapeCampId) }.returns(Mono.just(escapeCampAfterJeepersKeypersSecondRiddleSolved))
 
-            Si vous vous placez au bon endroit,
-            la réponse se révèlera.
-
-            """.trimIndent())
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        "riddle-1, solution-1, true",
-        "riddle-1, Solution-1, true",
-        "riddle-1, solution-2, false",
-        "unknown, solution-2, false")
-    fun `should check riddle solution`(riddleName: String, solution: String, expectedResult: Boolean) {
-        assertThat(gameService.isCorrect(riddleName, solution))
-            .isEqualTo(expectedResult)
+        gameService.gameLeaderBoard(escapeCampId)
+            .`as`(StepVerifier::create)
+            .assertNext {
+                assertThat(it).isEqualTo(
+                    LeaderBoard(
+                        escapeCampId,
+                        listOf(
+                            Line(lockedAndLoadedContestantName, 1, Duration.ofSeconds(464)),
+                            Line(jeepersKeypersContestantName, 2, Duration.ofSeconds(589))
+                        )
+                    )
+                )
+            }
+            .verifyComplete()
     }
 }
